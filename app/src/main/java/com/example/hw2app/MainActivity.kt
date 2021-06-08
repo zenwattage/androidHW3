b5ac57a5efe0173e6f9cf26ba15acf4c9073c60e
Scott Hansford <zenwattage@gmail.com>
2021-06-07 23:42:25 -0700

    initial push of cameramap activity

--- app/src/main/java/com/example/hw2app/MainActivity.kt
+++ app/src/main/java/com/example/hw2app/MainActivity.kt
@@ -3,7 +3,6 @@
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.hw2app.databinding.ActivityMainBinding
@@ -20,9 +19,15 @@ import com.example.hw2app.databinding.ActivityMainBinding
 
          val movieBtn = findViewById<Button>(R.id.movieBtn)
          val sendBtn = findViewById<Button>(R.id.sendBtn)
-         val pacBtn = findViewById<Button>(R.id.pacBtn)
+         val mapBtn = findViewById<Button>(R.id.pacBtn)
          val earthBtn = findViewById<Button>(R.id.earthBtn)
          val trafficBtn = findViewById<Button>(R.id.trafficBtn)
+
+
+         mapBtn.setOnClickListener {
+             // make a toast on button click event
+             Toast.makeText(this, "Hi there! This is a ${this.getString(R.string.pacBtn)} Button!", Toast.LENGTH_LONG).show()
+         }
 
          trafficBtn.setOnClickListener {
 //             Toast.makeText(this, "Hi there! This is a ${this.getString(R.string.nwBtn)} Button!", Toast.LENGTH_LONG).show()
@@ -40,10 +45,7 @@ import com.example.hw2app.databinding.ActivityMainBinding
              Toast.makeText(this, "Hi there! This is a ${this.getString(R.string.sendBtn)} Button.!", Toast.LENGTH_LONG).show()
          }
 
-         pacBtn.setOnClickListener {
-             // make a toast on button click event
-             Toast.makeText(this, "Hi there! This is a ${this.getString(R.string.pacBtn)} Button!", Toast.LENGTH_LONG).show()
-         }
+
 
          earthBtn.setOnClickListener {
              Toast.makeText(this, "Hi there! This is a ${this.getString(R.string.earthBtn)} Button!", Toast.LENGTH_LONG).show()
